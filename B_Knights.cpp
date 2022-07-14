d7380c1d75aeef2413ab3abb5fbc79027294ea65
joy-adhikary <joyadhikary074@gmail.com>
2022-07-14 22:36:07 +0600

    updated issue

---
+++ B_Knights.cpp
@@ -0,0 +1,128 @@
+/* 
+                            ॐ नमो नारायणाय
+
+                            ॐ नमः शिवाय  
+
+                            _Karn_2.0_
+                               
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<ll, ll> ml;
+typedef map<string, ll> ms;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+
+
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define pp pop_back
+#define mpp make_pair
+
+#define nn cout << '\n';
+#define NO cout << "NO" << endl;
+#define YES cout << "YES" << endl;
+#define no cout << "No" << endl;
+#define yes cout << "Yes" << endl;
+#define all(x) (x).begin(), (x).end()
+#define Sort(s) sort(s.begin(), s.end())
+
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+ 
+
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+bool sortbysec(const pair<string,ll> &a, const pair<string ,ll> &b)
+{
+    return (a.second > b.second);
+}
+// check the type of pair first 
+
+int main()
+{
+  return 0;
+
+}
